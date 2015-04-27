744425017bcaf9d491ff04494bdd574279fdfb20
Keith Horwood <keithwhor@gmail.com>
2015-04-27 11:25:04 -0700

    Fix CLI bug for generating projects

--- cli/cli.js
+++ cli/cli.js
@@ -1,42 +1,23 @@
 #!/usr/bin/env node
 
-var colors = require('colors/safe');
-var dbCommands = require('./db/commands.js');
-var generateCommands = require('./generate/commands.js');
-var fs = require('fs');
+(function() {
 
-var command = process.argv.slice(2, 3).pop();
+  var colors = require('colors/safe');
+  var fs = require('fs');
 
-command = command ? command : '_';
-command = {name: command.split(':')[0], value: command.split(':')[1] || '_'};
+  var command = process.argv.slice(2, 3).pop();
 
-if (command.name !== 'new' && !fs.existsSync(process.cwd() + '/.nodal')) {
-  console.error(colors.red.bold('Error: ') + 'Nodal project not initialized. Use `nodal new` to initialize a project.');
-  process.exit(0);
-}
+  command = command ? command : '_';
+  command = {name: command.split(':')[0], value: command.split(':')[1] || '_'};
 
-var args = [];
-var flags = {};
+  if (command.name !== 'new' && !fs.existsSync(process.cwd() + '/.nodal')) {
 
-process.argv.slice(3).forEach(function(v) {
-  var values = v.split(':');
-  if (v.substr(0, 2) === '--') {
-    values[0] = values[0].substr(2);
-    flags[values[0]] = values[1];
-  } else {
-    args.push(values);
-  }
-});
+    console.error(colors.red.bold('Error: ') + 'Nodal project not initialized. Use `nodal new` to initialize a project.');
+    process.exit(0);
 
-var commands = {
-  _: {
-    _: function(args, flags) {
-      console.error(colors.red.bold('Error: ') + 'No command specified');
-      process.exit(0);
-    }
-  },
-  new: {
-    _: function(args, flags) {
+  } else if (command.name === 'new') {
+
+    (function() {
 
       var ncp = require('ncp');
 
@@ -45,58 +26,85 @@ var commands = {
           return console.error(err);
         }
         console.log('Created new nodal project!');
+        process.exit(0);
       });
 
-    }
-  },
-  s: {
-    _: function(args, flags) {
+    })();
 
-      var Nodal = require('../core/module.js');
-      var watch = require('watch');
+    return;
 
-      var restart = false;
-
-      var spawn = require('child_process').spawn;
-      var child = spawn('npm',  ['start'], {stdio: [process.stdin, process.stdout, process.stderr]});
-
-      process.on('exit', function() {
-        child && child.kill('SIGINT');
-      });
-
-    }
-  },
-  db: {
-    _: function(args, flags) {
-      console.log('Please specify an action');
-    },
-    create: dbCommands.create,
-    drop: dbCommands.drop,
-    prepare: dbCommands.prepare,
-    migrate: dbCommands.migrate,
-    rollback: dbCommands.rollback
-  },
-  g: {
-    _: function(args, flags) {
-      console.log('Please specify an action');
-    },
-    model: generateCommands.model,
-    migration: generateCommands.migration
   }
-};
 
-var cmd = commands[command.name];
-var subCmd;
+  var args = [];
+  var flags = {};
 
-if (cmd) {
-  subCmd = cmd[command.value];
-  if (subCmd) {
-    subCmd(args, flags);
+  process.argv.slice(3).forEach(function(v) {
+    var values = v.split(':');
+    if (v.substr(0, 2) === '--') {
+      values[0] = values[0].substr(2);
+      flags[values[0]] = values[1];
+    } else {
+      args.push(values);
+    }
+  });
+
+  var commands = {
+    _: {
+      _: function(args, flags) {
+        console.error(colors.red.bold('Error: ') + 'No command specified');
+        process.exit(0);
+      }
+    },
+    s: {
+      _: function(args, flags) {
+
+        var Nodal = require('../core/module.js');
+        var watch = require('watch');
+
+        var restart = false;
+
+        var spawn = require('child_process').spawn;
+        var child = spawn('npm',  ['start'], {stdio: [process.stdin, process.stdout, process.stderr]});
+
+        process.on('exit', function() {
+          child && child.kill('SIGINT');
+        });
+
+      }
+    },
+    db: {
+      _: function(args, flags) {
+        console.log('Please specify an action');
+      },
+      create: dbCommands.create,
+      drop: dbCommands.drop,
+      prepare: dbCommands.prepare,
+      migrate: dbCommands.migrate,
+      rollback: dbCommands.rollback
+    },
+    g: {
+      _: function(args, flags) {
+        console.log('Please specify an action');
+      },
+      model: generateCommands.model,
+      migration: generateCommands.migration
+    }
+  };
+
+  var cmd = commands[command.name];
+  var subCmd;
+
+  if (cmd) {
+    subCmd = cmd[command.value];
+    if (subCmd) {
+      subCmd(args, flags);
+    } else {
+      console.error(colors.red.bold('Error: ') + 'Sub command "' + command.value + '" of "' + command.name + '" not found');
+      process.exit(0);
+    }
   } else {
-    console.error(colors.red.bold('Error: ') + 'Sub command "' + command.value + '" of "' + command.name + '" not found');
+    console.error(colors.red.bold('Error: ') + 'Command "' + command.name + '" not found');
     process.exit(0);
   }
-} else {
-  console.error(colors.red.bold('Error: ') + 'Command "' + command.name + '" not found');
-  process.exit(0);
-}
+
+})();
