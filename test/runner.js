7ba7a4c38c5f4980a30882ffff0c89791dcc5451
Schahriar SaffarShargh <info@schahriar.com>
2016-01-10 12:39:13 -0800

    Fully separated OS implementations
    Only way to get it to work on both Windows & Linux (I haven't tested OSX
    but I am assuming it would work since the fallback code is the original)

--- test/runner.js
+++ test/runner.js
@@ -10,56 +10,56 @@ describe('Test Suite', function() {
   let Nodal = require('../core/module.js');
   Nodal.rootDirectory = __dirname;
   
-  // `psql` can take a long time to respond to a request on Windows
-  // Here we pass a ~15 seconds timeout to allow for the
-  // child process to exit gracefully or timeout.
-  let processOptions = {
-    timeout: 14900
-  };
-  
-  let commandTable = {
-    "drop": 'psql -c \'drop database if exists nodal_test;\' -U postgres',
-    "create": "psql -c \'create database nodal_test;\' -U postgres",
-    "WIN32": {
-      "drop": 'psql -q -c "drop database if exists nodal_test;" -U postgres',
-      "create": 'psql -a -c "create database nodal_test;" -U postgres'
-    }
-  };
-  
-  let getOSCommand = function (command) {
-    if(os.platform() === 'win32') {
-      return commandTable.WIN32[command];
-    }else{
-      return commandTable[command];
-    }
-  };
+  if(os.platform() === 'win32') {
+    // `psql` can take a long time to respond to a request on Windows
+    // Here we pass a ~15 seconds timeout to allow for the
+    // child process to exit gracefully or timeout.
+    let processOptions = {
+      timeout: 14900
+    };
+    
+    before(function(done) {
+      this.timeout(30000); // Set timeout to 30 seconds
+      if (global.env === 'development') {
+        // Using async exec here to easily handler stderr
+        // Errors are not thrown and instead are treated as warnings
+        child_process.exec('psql -q -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+          if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
+          // 
+          child_process.exec('psql -a -c "create database nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+            if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
+            done();
+          });
+        });
+      }
+    });
 
-  before(function(done) {
-    this.timeout(30000); // Set timeout to 30 seconds
-    if (global.env === 'development') {
-      // Using async exec here to easily handler stderr
-      // Errors are not thrown and instead are treated as warnings
-      child_process.exec(getOSCommand("drop"), processOptions, function(error, stdout, stderr) {
-        if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
-        // 
-        child_process.exec(getOSCommand("create"), processOptions, function(error, stdout, stderr) {
+    after(function(done) {
+      this.timeout(30000); // Set timeout to 30 seconds
+      if (global.env === 'development') {
+        // Don't remove the -q option, it will break the db connection pool
+        child_process.exec('psql -q -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
           if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
           done();
         });
-      });
-    }
-  });
+      }
+    });
+  }else{
+    before(function() {
+      if (global.env === 'development') {
+        // child_process.execSync('createuser postgres -s -q');
+        child_process.execSync('psql -c \'drop database if exists nodal_test;\' -U postgres');
+        child_process.execSync('psql -c \'create database nodal_test;\' -U postgres');
+      }
+    });
 
-  after(function(done) {
-    this.timeout(30000); // Set timeout to 30 seconds
-    if (global.env === 'development') {
-      // Don't remove the -q option, it will break the db connection pool
-      child_process.exec(getOSCommand("drop"), processOptions, function(error, stdout, stderr) {
-        if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
-        done();
-      });
-    }
-  });
+    after(function() {
+      if (global.env === 'development') {
+        child_process.execSync('psql -c \'drop database if exists nodal_test;\' -U postgres');
+      }
+    });
+  }
+  
 
   require('./tests/nodal.js')(Nodal);
 
