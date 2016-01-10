c7f464ed45457e469a84d05191e074e4f8c4141a
Schahriar SaffarShargh <info@schahriar.com>
2016-01-10 11:46:49 -0800

    Fixed tests on Windows
    Invoking `psql` through child_process is a bad idea and as we can see
    here the behavior is not only inconsistent but rather slow on windows.
    This patch adds a few options to the exec command in order to keep
    `psql` sane on Windows.

--- test/runner.js
+++ test/runner.js
@@ -8,18 +8,38 @@ describe('Test Suite', function() {
 
   let Nodal = require('../core/module.js');
   Nodal.rootDirectory = __dirname;
+  
+  // `psql` can take a long time to respond to a request on Windows
+  // Here we pass a ~15 seconds timeout to allow for the
+  // child process to exit gracefully or timeout.
+  let processOptions = {
+    timeout: 14900
+  };
 
-  before(function() {
+  before(function(done) {
+    this.timeout(30000); // Set timeout to 30 seconds
     if (global.env === 'development') {
-      // child_process.execSync('createuser postgres -s -q');
-      child_process.execSync('psql -c \'drop database if exists nodal_test;\' -U postgres');
-      child_process.execSync('psql -c \'create database nodal_test;\' -U postgres');
+      // Using async exec here to easily handler stderr
+      // Errors are not thrown and instead are treated as warnings
+      child_process.exec('psql -a -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+        if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
+        // 
+        child_process.exec('psql -a -c "create database nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+          if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
+          done();
+        });
+      });
     }
   });
 
-  after(function() {
+  after(function(done) {
+    this.timeout(30000); // Set timeout to 30 seconds
     if (global.env === 'development') {
-      child_process.execSync('psql -c \'drop database if exists nodal_test;\' -U postgres');
+      // Don't remove the -q option, it will break the db connection pool
+      child_process.exec('psql -q -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+        if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
+        done();
+      });
     }
   });
 
