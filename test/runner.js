f74ba494daded5bafbe88674bb4bec43725e0f6a
Schahriar SaffarShargh <info@schahriar.com>
2016-01-10 11:55:38 -0800

    Made `exec` commands OS specific

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let child_process = require('child_process');
+let os = require('os');
 
 global.env = process.env.NODE_ENV || 'development';
 
@@ -15,16 +16,33 @@ describe('Test Suite', function() {
   let processOptions = {
     timeout: 14900
   };
+  
+  let commandTable = {
+    "drop": 'psql -c \'drop database if exists nodal_test;\' -U postgres',
+    "create": "psql -c \'create database nodal_test;\' -U postgres",
+    "WIN32": {
+      "drop": 'psql -q -c "drop database if exists nodal_test;" -U postgres',
+      "create": 'psql -a -c "create database nodal_test;" -U postgres'
+    }
+  };
+  
+  let getOSCommand = function (command) {
+    if(os.platform() === 'win32') {
+      return commandTable.WIN32[command];
+    }else{
+      return commandTable[command];
+    }
+  };
 
   before(function(done) {
     this.timeout(30000); // Set timeout to 30 seconds
     if (global.env === 'development') {
       // Using async exec here to easily handler stderr
       // Errors are not thrown and instead are treated as warnings
-      child_process.exec('psql -a -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+      child_process.exec(getOSCommand("drop"), processOptions, function(error, stdout, stderr) {
         if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
         // 
-        child_process.exec('psql -a -c "create database nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+        child_process.exec(getOSCommand("create"), processOptions, function(error, stdout, stderr) {
           if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
           done();
         });
@@ -36,7 +54,7 @@ describe('Test Suite', function() {
     this.timeout(30000); // Set timeout to 30 seconds
     if (global.env === 'development') {
       // Don't remove the -q option, it will break the db connection pool
-      child_process.exec('psql -q -c "drop database if exists nodal_test;" -U postgres', processOptions, function(error, stdout, stderr) {
+      child_process.exec(getOSCommand("drop"), processOptions, function(error, stdout, stderr) {
         if(error) console.warn("Warning:", stderr, "\nErrors ignored.");
         done();
       });
